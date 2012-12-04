c257cf530cd500d136122f68ce99937e98da3c34
Pavel Ivanchenko <IvanchenkoPavel1186@gmail.com>
2012-12-04 16:07:57 +0200

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = '372d546cc19ad4980536abc93b62f27d15559b627010976aa2f2e5a0435badbcfbe7de26b19589562ddf7626c078947c10cee2177cbc7ebae9630df11eac472a'
